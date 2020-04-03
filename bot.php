47982e69c9d8967574cd6bf0ca523a18b37582b0
Momotari <46018667+NumberNine9s@users.noreply.github.com>
2020-04-03 21:37:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,14 +27,13 @@ if (!is_null($events['events'])) {
             ];
             if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
             // Get text sent
-            $text = $event['message']['sticker'];
+            $sticker = $event['message']['sticker'];
             // Get replyToken
             $replyToken = $event['replyToken'];
             // Build message to reply back
             $messages = [
                 'type' => 'sticker',
-                'packageId' => 1,
-                "stickerId": "2576",
+                'sticker' => $sticker,
                
             ];
             // Make a POST Request to Messaging API to reply to sender
