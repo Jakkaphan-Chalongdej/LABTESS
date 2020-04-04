41dbf94916b130865b461afaf02ecf5f1b3543c7
Momotari <46018667+NumberNine9s@users.noreply.github.com>
2020-04-04 13:05:29 +0700

    Update api-i.php

--- api-i.php
+++ api-i.php
@@ -9,8 +9,8 @@ $dbName = "chatbot";
 $connect = mysqli_connect($serverName, $userName, $userPassword, $dbName) or die("connect error" . mysqli_error());
 mysqli_set_charset($connect, "utf8");*/
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'lhh5bqVWFDXPY+cPsIeJrf9vC06L4SesNGP2SkzJjCdSgawBODTHK5tSZWjPiBytVm3QAkbOq8RAsIonUVszGz6Ok02qnDGZLKAtF3ltP9shw7EdD1FimHhpM/AzI1Bjvhbf0zu0TqEgOO7dBmSG9QdB04t89/1O/w1cDnyilFU='; // Access Token จาก Line developer
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer $ACCESS_TOKEN');
+$ACCESS_TOKEN = 'AJeYHUJbVy6d/VeCU+yJ7ShyCtmMl8yUv1LJPjDGFjIvSTBDgBtYp/0/VX6QJtONwP1CLuWMZCsAIb7ZI4JPPe2FADwvipoxDrpqBqypvBZ47D9vvObZ0C4oXgc8pnIHTpxc/TtDAdy+2swhHhD5wgdB04t89/1O/w1cDnyilFU='; // Access Token จาก Line developer
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer AJeYHUJbVy6d/VeCU+yJ7ShyCtmMl8yUv1LJPjDGFjIvSTBDgBtYp/0/VX6QJtONwP1CLuWMZCsAIb7ZI4JPPe2FADwvipoxDrpqBqypvBZ47D9vvObZ0C4oXgc8pnIHTpxc/TtDAdy+2swhHhD5wgdB04t89/1O/w1cDnyilFU=');
 $request = file_get_contents('php://input');
 $request_array = json_decode($request, true);
 if (sizeof($request_array['events']) > 0) {
@@ -21,15 +21,15 @@ if (sizeof($request_array['events']) > 0) {
         if ($event['type'] == 'message') {
             $post = [
                 "http" => [
-                    "header" => "Content-Type: application/json\r\n" . 'Authorization: Bearer $ACCESS_TOKEN',
+                    "header" => "Content-Type: application/json\r\n" . 'Authorization: Bearer AJeYHUJbVy6d/VeCU+yJ7ShyCtmMl8yUv1LJPjDGFjIvSTBDgBtYp/0/VX6QJtONwP1CLuWMZCsAIb7ZI4JPPe2FADwvipoxDrpqBqypvBZ47D9vvObZ0C4oXgc8pnIHTpxc/TtDAdy+2swhHhD5wgdB04t89/1O/w1cDnyilFU=',
                 ],
             ];
         }
         $context = stream_context_create($post);
-        //$profile_json = file_get_contents('https://api.line.me/v2/bot/profile/' . $user_id, false, $context);
-        //$profile_array = json_decode($profile_json, true);
-        //$pic_ = $profile_array[pictureUrl];
-        //$name_ = $profile_array[displayName];
+        $profile_json = file_get_contents('https://api.line.me/v2/bot/profile/' . $user_id, false, $context);
+        $profile_array = json_decode($profile_json, true);
+        $pic_ = $profile_array[pictureUrl];
+        $name_ = $profile_array[displayName];
         
         if ($event['message']['type'] == 'text') {
             $text = $event['message']['text'];
@@ -40,7 +40,7 @@ if (sizeof($request_array['events']) > 0) {
             fclose($myfile); */
             /*$query = "INSERT INTO chatbot_log(user_id,name,pic,text,date_time) VALUE ('$user_id','$name_','$pic_' ,'$text',NOW())";
             $resource = mysqli_query($connect, $query) or die(“error” . mysqli_error());*/
-            $url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/29f0ffe2-835f-480d-8410-2351e7e5ff17";
+            $url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/f53b38db-d98d-41d6-914c-86726c8e3485";
             $headers = getallheaders();
             $headers['Host'] = "bots.dialogflow.com";
             $json_headers = array();
@@ -69,7 +69,7 @@ if (sizeof($request_array['events']) > 0) {
             fclose($myfile); */
             /*$query = "INSERT INTO chatbot_log(user_id,name,pic,text,date_time) VALUE ('$user_id','$name_','$pic_' ,'$text',NOW())";
             $resource = mysqli_query($connect, $query) or die(“error” . mysqli_error());*/
-            $url = "https://abdul.in.th/callback/2e31437050351d49f8befaa04ed296ed.php";
+            $url = "https://abdul.in.th/callback/9b30aacdf0cd3dbcbdef08c1c948775b.php";
             $headers = getallheaders();
             $headers['Host'] = "abdul.in.th";
             $json_headers = array();
