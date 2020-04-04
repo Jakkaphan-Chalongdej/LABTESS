6f83980bdab12fd8d75fe48361f349a53e932751
Momotari <46018667+NumberNine9s@users.noreply.github.com>
2020-04-04 19:11:37 +0700

    Update api-i.php

--- api-i.php
+++ api-i.php
@@ -11,7 +11,7 @@
 
 <?php
 date_default_timezone_set("Asia/Bangkok");
-$date_ = date("Y-m-d");
+$date_ = date("d-m-Y");
 $time_ = date("H:i:s");
 $serverName = "test1.csw86ar6olyd.us-west-2.rds.amazonaws.com";
 $userName = "admin";
